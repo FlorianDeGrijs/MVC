2247c25a930dab3528f1c186ac8286bfd3fac43f
Florian <13017799@student.hhs.nl>
2019-12-08 18:18:32 +0100

    Add project files.

---
+++ Controllers/MathController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Form.Controllers
+{
+    public class MathController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Verwerken(string Naam, int Geboortejaar)
+        {
+            if (Geboortejaar < 1998)
+            {
+                ViewData["Message"] = "Hartelijk dank meneer/mevrouw " + Naam;
+            }
+            else
+            {
+                ViewData["Message"] = "Helaas bent u nog te jong.";
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/StudentenAdministratieController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Form.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Form.Controllers
+{
+    public class StudentenAdministratieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ZoekStudenten(string id)
+        {
+            if (id == null)
+            {
+                return View("Index");
+            }
+
+            ViewData["CharacterSearched"] = id;
+
+            List<Student> students = new List<Student>() {
+            new Student() {Name = "John", Age = 19, Hobby = "Play WoW"},
+            new Student() {Name = "Jamie", Age = 21, Hobby = "Play Minecraft"},
+            new Student() {Name = "Phillip", Age = 18, Hobby = "Play Fortnite"},
+            new Student() {Name = "Flo", Age = 25, Hobby = "Play LoL"},
+            };
+
+            List<Student> searchStudents = students
+                .Where(s => s.Name.ToLower().StartsWith(id.ToLower()))
+                .OrderBy(s => s.Name)
+                .ToList();
+
+            if (!searchStudents.Any())
+            {
+                return NotFound();
+            }
+
+            return View(searchStudents);
+        }
+
+    }
+}
